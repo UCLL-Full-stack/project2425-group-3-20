00206aab4cb9925507412fa1aed913f4ba9a94d4
Rune-VanBouwelBrams <rune.vanbouwelbrams@student.ucll.be>
2024-10-10 11:44:52 +0200

    Created Action class

---
+++ back-end/model/action.ts
@@ -0,0 +1,44 @@
+import { ActionType } from "../types";
+
+export class Action {
+    private id?: number;
+    private name: string;
+    private description: string;
+    private type: ActionType;
+
+    constructor(action: {name: string, description: string, type: ActionType}) {
+        this.name = action.name;
+        this.description = action.description;
+        this.type = action.type;
+    }
+
+    // getters:
+    getId(): number | undefined {
+        return this.id;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    getDescription(): string {
+        return this.description;
+    }
+
+    getType(): ActionType {
+        return this.type;
+    }
+
+    //setters:
+    setName(name: string): void {
+        this.name = name;
+    }
+
+    setDescription(description: string): void {
+        this.description = description;
+    }
+
+    setType(type: ActionType): void {
+        this.type = type;
+    }
+}
