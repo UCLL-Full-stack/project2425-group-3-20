6721065eecdb261ac68dd958a74663d9a801d1e2
JorritR <jorrit.ruelens@student.ucll.be>
2024-11-16 12:21:43 +0100

    Create Action Create monster in last commit

--- back-end/repository/action.db.ts
+++ back-end/repository/action.db.ts
@@ -1,9 +1,29 @@
 import { Action } from "../model/action";
+import { ActionType } from "../types";
 import database from "../util/database";
+import { 
+    Action as ActionPrisma
+} from '@prisma/client';
 
 
+const createAction = async ({ name, description, type }:ActionPrisma ): Promise<Action> => {
+    try {
+        // Insert the new action into the database
+        const actionPrisma = await database.action.create({
+            data: {
+                name,
+                description,
+                type,
+            },
+        });
 
-
+        // Return the created action as an instance of the Action class
+        return new Action(actionPrisma);
+    } catch (error) {
+        console.error(error);
+        throw new Error('Database Error. See server logs');
+    }
+};
 const getAllActions = async (): Promise<Action[]> => {
     try{
         const actionsPrisma = await database.action.findMany();
@@ -29,4 +49,4 @@ const getActionById = async (actionId: number): Promise<Action | null> => {
     }
 };
 
-export default { getAllActions, getActionById };
+export default { getAllActions, getActionById, createAction };
